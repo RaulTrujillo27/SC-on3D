3825ca850689f0bd6362e4328fb8523069fe8984
RaulTrujillo27 <raultrujillo27@gmail.com>
2023-05-23 16:47:04 +0200

    normalize data

--- format.js
+++ format.js
@@ -1,3 +1,37 @@
+
+
+function normalizardatos(matrix){
+  const columnMeans = [];
+  for (let col = 0; col < matrix[0].length; col++) {
+    let sum = 0;
+    for (let row = 0; row < matrix.length; row++) {
+      sum += matrix[row][col];
+    }
+    const mean = sum / matrix.length;
+    columnMeans.push(mean);
+  }
+
+  const columnStdDeviations = [];
+  for (let col = 0; col < matrix[0].length; col++) {
+    let squaredDiffSum = 0;
+    for (let row = 0; row < matrix.length; row++) {
+      const diff = matrix[row][col] - columnMeans[col];
+      squaredDiffSum += diff * diff;
+    }
+    const variance = squaredDiffSum / matrix.length;
+    const stdDeviation = Math.sqrt(variance);
+    columnStdDeviations.push(stdDeviation);
+  }
+  for (let col = 0; col < matrix[0].length; col++) {
+    for (let row = 0; row < matrix.length; row++) {
+      matrix[row][col] = (matrix[row][col] - columnMeans[col]) / columnStdDeviations[col];      
+    }
+  }
+
+  return matrix;
+}
+
+
 function multiply(a, b) {
   var aNumRows = a.length, aNumCols = Object.values(a[0]).length,
       bNumRows = b.length, bNumCols = Object.values(b[0]).length,
@@ -10,11 +44,20 @@ function multiply(a, b) {
           m[r][c] += Object.values(a[r])[i] * Object.values(b[i])[c];            
       }      
     }
-    m[r][3] =1; 
+        
+    
+  }
+  m = normalizardatos(m);
+  for (var r = 0; r < aNumRows; ++r) {
+    m[r][2] = m[r][2] + 10;
+    m[r][3] =1;
     m[r] = Object.assign({},m[r]);
   }
   return m;
 }
+
+
+
 AFRAME.registerComponent('multiply-matrix', {
     schema:{
         archivo: { type:'string',defaul:''},
@@ -24,12 +67,7 @@ AFRAME.registerComponent('multiply-matrix', {
       var  archivo = this.data.archivo;
       var  matriz = this.data.matriz;
       this.el.addEventListener('click', function(){
-            //------------------------------------------------- 
-            var eso = this;
             var burbujas = document.querySelector('#bubblesrealdata');
-            //var datos1 = document.querySelectorAll('[babia-queryjson]');
-            //burbujas.setAttribute('babia-bubbles','from',datos1[1].id);
-            //-------------------------------------------------     
             fetch(archivo)
               .then(function(response) {
                 return response.json();
@@ -41,6 +79,7 @@ AFRAME.registerComponent('multiply-matrix', {
                   })
                   .then(function(matrix) {
                     var m  = multiply(archive,matrix);
+                    console.log(m);
                     burbujas.setAttribute('babia-bubbles','data',JSON.stringify(m));
                   });
               });
@@ -49,4 +88,5 @@ AFRAME.registerComponent('multiply-matrix', {
       
     }             
   });
+
 
